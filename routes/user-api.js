289ebb25eeeee698f3711ba793fc5ee3398221b5
Sam <sammynarduzzi@gmail.com>
2023-03-21 14:52:57 -0700

    add new route and create basic query for cart to start

--- routes/user-api.js
+++ routes/user-api.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router  = express.Router();
 const menuQueries = require('../db/queries/menus')
+const cartQueries = require('../db/queries/cart')
+
 
 router.get('/menus', (req, res) => {
   menuQueries.getMenus()
     .then(items => {
       res.json({ items });
     })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.get('/cart', (req, res) => {
+
+  cartQueries.getCart()
+    .then(items => {
+      console.log(items)
+      res.json({ items });
+    })
+
     .catch(err => {
       res
         .status(500)
