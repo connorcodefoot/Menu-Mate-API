67b6c49ed1b89725c591edcfbd521cba9a93b69a
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-03-21 07:47:11 -0700

    Update query for orders to return order item details

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -1,9 +1,23 @@
 const db = require('../connection');
 
 const getOrders = () => {
-  return db.query('SELECT * FROM orders;')
+  return db.query(
+    `
+    SELECT
+    orders.id AS orderID,
+    orders.customer_name AS customer,
+    orders.table_number AS table,
+    order_items.id AS orderItemID,
+    items.title AS item,
+    items.price_cents AS itemPrice
+    FROM orders
+    JOIN order_items ON order_items.order_id = orders.id
+    JOIN items ON items.id = order_items.item_id
+    GROUP BY orderID, order_items.id, items.id
+    `
+    )
     .then(data => {
-      return data.rows;
+      return data.rows
     });
 };
 
