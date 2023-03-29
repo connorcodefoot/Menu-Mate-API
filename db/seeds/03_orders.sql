8b6cfdf6773149aeab49a9ef8993f8df9d07c566
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-03-29 09:00:57 -0700

    Update orders to show appropriate numbers

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,14 +1,14 @@
-INSERT INTO orders (customer_name, table_number, order_total_cents) VALUES ('Rachel', 2, 32);
+INSERT INTO orders (customer_name, table_number, order_total_cents) VALUES ('Rachel', 2, 3200);
 
-INSERT INTO orders (customer_name, table_number, order_total_cents, order_status) VALUES ('Joey', 2, 16, 'Done');
+INSERT INTO orders (customer_name, table_number, order_total_cents, order_status) VALUES ('Joey', 2, 1600, 'Done');
 
-INSERT INTO orders (customer_name, table_number, order_total_cents, paid, order_status) VALUES ('Chandler', 7, 16, 1, 'In Progress');
-
-
-INSERT INTO orders (customer_name, table_number, order_total_cents, order_status) VALUES ('Monica', 7, 15, 'Done');
+INSERT INTO orders (customer_name, table_number, order_total_cents, paid, order_status) VALUES ('Chandler', 7, 1600, 1, 'In Progress');
 
 
-INSERT INTO orders (customer_name, table_number, order_total_cents, paid) VALUES ('Ross', 5, 14, 1);
+INSERT INTO orders (customer_name, table_number, order_total_cents, order_status) VALUES ('Monica', 7, 1500, 'Done');
+
+
+INSERT INTO orders (customer_name, table_number, order_total_cents, paid) VALUES ('Ross', 5, 1400, 1);
 
 
 
